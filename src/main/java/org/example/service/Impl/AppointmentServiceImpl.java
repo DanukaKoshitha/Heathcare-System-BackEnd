3d803ddd2e2ade8ed3fbbc0b60148fc192ad05db
Danuka <danukakoshitha2004@gmail.com>
2025-03-20 03:57:32 +0530

    Create the Appointment Service Implementation Class.

---
+++ src/main/java/org/example/service/Impl/AppointmentServiceImpl.java
@@ -0,0 +1,32 @@
+package org.example.service.Impl;
+
+import org.example.dto.Appointment;
+import org.example.service.AppointmentService;
+import java.util.List;
+
+public class AppointmentServiceImpl implements AppointmentService {
+    @Override
+    public void addAppointment(Appointment appointment) {
+
+    }
+
+    @Override
+    public List<Appointment> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Appointment searchAppointment(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void updateAppointment(Appointment appointment) {
+
+    }
+
+    @Override
+    public void deleteAppointment(Integer id) {
+
+    }
+}
